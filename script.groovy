e2c9440fdade5bacf9b94638412fe533cf209381
Denys Podobrii <denys.podobrii@gmail.com>
2024-08-23 10:32:37 +0300

    Fix credentialId

--- script.groovy
+++ script.groovy
@@ -9,7 +9,7 @@ def testApp() {
 }
 
 def buildImage() {
-    withCredentials([usernamePassword(credentialsID: 'docker-hub', usernameVariable: 'USER', passwordVariable: 'PWD')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker-hub', usernameVariable: 'USER', passwordVariable: 'PWD')]) {
         sh 'docker build -t dennydobry/test-java-app:jma-1.2 .'
         sh 'echo $PWD | docker login -u $USER --password-input'
         sh 'docker push dennydobry/test-java-app:jma-1.2'
